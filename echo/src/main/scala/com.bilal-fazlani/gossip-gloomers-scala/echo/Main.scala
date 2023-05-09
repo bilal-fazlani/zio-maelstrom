b4f64d961a8a8e652988117f52ce22366bd263c1
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-05-09 22:48:20 +0530

    simplify echo example

--- echo/src/main/scala/com.bilal-fazlani/gossip-gloomers-scala/echo/Main.scala
+++ echo/src/main/scala/com.bilal-fazlani/gossip-gloomers-scala/echo/Main.scala
@@ -7,19 +7,14 @@ import zioMaelstrom.protocol.*
 import zio.*
 
 object Main extends MaelstromNode[Echo, EchoOk]:
-
-  def handle(in_msg: Message[Echo]) =
+  def handle(in: Message[Echo]) =
     for {
-      newMessageId <- Random.nextIntBetween(1, Int.MaxValue).map(MessageId.apply)
-      _ <- in_msg reply EchoOk(echo = in_msg.body.echo, msg_id = newMessageId, in_reply_to = in_msg.body.msg_id)
+      newId <- Random.nextIntBetween(1, Int.MaxValue).map(MessageId.apply)
+      out = EchoOk(echo = in.body.echo, msg_id = newId, in_reply_to = in.body.msg_id)
+      _ <- in reply out
     } yield ()
 
-@jsonDiscriminator("type")
-sealed trait EchoProtocol extends MessageBody
+case class Echo(echo: String, msg_id: MessageId, `type`: String = "echo") extends MessageWithId derives JsonDecoder
 
-@jsonHint("echo")
-case class Echo(echo: String, msg_id: MessageId, `type`: String = "echo") extends EchoProtocol, MessageWithId derives JsonDecoder
-
-@jsonHint("echo_ok")
 case class EchoOk(echo: String, msg_id: MessageId, in_reply_to: MessageId, `type`: String = "echo_ok") extends MessageWithId, MessageWithReply
     derives JsonEncoder
