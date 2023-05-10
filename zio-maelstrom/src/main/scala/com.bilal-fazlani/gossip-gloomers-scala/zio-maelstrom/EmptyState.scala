2c267e51e60f3a0605fb28e9a7dca91abff2e15a
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-05-10 21:43:36 +0530

    use val instead of def for empty state value

--- zio-maelstrom/src/main/scala/com.bilal-fazlani/gossip-gloomers-scala/zio-maelstrom/EmptyState.scala
+++ zio-maelstrom/src/main/scala/com.bilal-fazlani/gossip-gloomers-scala/zio-maelstrom/EmptyState.scala
@@ -2,45 +2,45 @@ package com.bilalfazlani.gossipGloomersScala
 package zioMaelstrom
 
 trait EmptyState[+S]:
-  def emptyValue: S
+  val emptyValue: S
 
 object EmptyState {
 
   def apply[S](using z: EmptyState[S]): S = z.emptyValue
 
   given EmptyState[Unit] = new EmptyState[Unit] {
-    def emptyValue = ()
+    val emptyValue = ()
   }
 
   given EmptyState[Int] = new EmptyState[Int] {
-    def emptyValue = 0
+    val emptyValue = 0
   }
 
   given EmptyState[Long] = new EmptyState[Long] {
-    def emptyValue = 0L
+    val emptyValue = 0L
   }
 
   given EmptyState[Float] = new EmptyState[Float] {
-    def emptyValue = 0.0f
+    val emptyValue = 0.0f
   }
 
   given EmptyState[Double] = new EmptyState[Double] {
-    def emptyValue = 0.0
+    val emptyValue = 0.0
   }
 
   given EmptyState[Boolean] = new EmptyState[Boolean] {
-    def emptyValue = false
+    val emptyValue = false
   }
 
   given EmptyState[String] = new EmptyState[String] {
-    def emptyValue = ""
+    val emptyValue = ""
   }
 
   given [A]: EmptyState[Option[A]] = new EmptyState[Option[A]] {
-    def emptyValue = None
+    val emptyValue = None
   }
 
   given [A]: EmptyState[Seq[A]] = new EmptyState[Seq[A]] {
-    def emptyValue = Seq.empty
+    val emptyValue = Seq.empty
   }
 }
