2ae3be8a127bfb510e385e2f8383c1133090528b
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-05-17 00:39:56 +0530

    some refactoring to initializer

--- zio-maelstrom/src/main/scala/com.bilal-fazlani/gossip-gloomers-scala/zio-maelstrom/Initializer.scala
+++ zio-maelstrom/src/main/scala/com.bilal-fazlani/gossip-gloomers-scala/zio-maelstrom/Initializer.scala
@@ -14,7 +14,9 @@ trait Initializer:
   def initialize(inputStream: ZStream[Any, Throwable, String]): ZIO[Scope, Throwable, (Context, ZStream[Any, Throwable, GenericMessage])]
 
 object Initializer:
-  def initialize(inputStream: ZStream[Any, Throwable, String]): ZIO[Scope & Initializer, Throwable, (Context, ZStream[Any, Throwable, GenericMessage])] =
+  def initialize(
+      inputStream: ZStream[Any, Throwable, String]
+  ): ZIO[Scope & Initializer, Throwable, (Context, ZStream[Any, Throwable, GenericMessage])] =
     ZIO.serviceWithZIO[Initializer](_.initialize(inputStream))
 
   val live = ZLayer.fromFunction(InitializerLive.apply)
@@ -22,35 +24,45 @@ object Initializer:
 case class InitializerLive(debugger: Debugger, transport: MessageTransport) extends Initializer:
   def initialize(inputStream: ZStream[Any, Throwable, String]): ZIO[Scope, Throwable, (Context, ZStream[Any, Throwable, GenericMessage])] =
     inputStream
+      .tap(str => debugger.debugMessage(s"received LINE message: $str"))
       .map(str => (str, JsonDecoder[GenericMessage].decodeJson(str)))
-      .collectZIO {
-        // right means a valid json of a generic message
-        case (str, Right(genericMessage)) =>
-          if genericMessage isOfType "init"
-          then // if init, then decode it as init message
-            val initMessage = JsonDecoder[Message[MaelstromInit]].decodeJson(str)
-            initMessage match
-              // when decoded, send it to init handler
-              case Right(initMessage) => handleInit(initMessage) as Some(genericMessage)
-              // if decoding failed, send an error message to sender and log error
-              case Left(error) => handleInitDecoding(genericMessage) as None
-          else // else send an error message to sender and log error
-            handleMessageOtherThanInit(genericMessage) as None
-
-        // left means invalid json or missing bare minimum fields
-        // cant reply to anyone. just log the error
-        // in this case, this stream will never end and node will be in initialising state forever
-        case (input, Left(error)) =>
-          debugger.debugMessage(s"expected init message. recieved invalid input. error: $error, input: $input") as None
+      .map[(String, Either[String, GenericMessage], Boolean)] {
+        case (str, Right(genericMessage)) => (str, Right(genericMessage), genericMessage isOfType "init")
+        case (str, Left(error))           => (str, Left(error), false)
       }
-      .takeUntil(_.isDefined)
-      .collectSome // keep only init message
+      .find(_._3)
       .peel(ZSink.head)
-      // stream is now completed.
-      // if we don't have a sum yet, it means we didn't get any init message
-      // since we can't proceed without init message, its safe to fail the program
-      .collect(Exception("no init message received")) { case (Some(genericMessage), remainder) =>
-        (Context(MaelstromInit.parseInitUnsafe(genericMessage)), remainder)
+      .flatMap {
+        // happy case: found init message
+        case (Some(input, Right(genericMessage), _), remainder) =>
+          val initMessage = JsonDecoder[Message[MaelstromInit]].decodeJson(input)
+          initMessage match
+            // when decoded, send it to init handler
+            case Right(initMessage) =>
+              handleInit(initMessage) as (Context(initMessage), remainder.collectZIO {
+                case (_, Right(genericMessage), _) => ZIO.succeed(Some(genericMessage))
+                case (str, Left(error), _)         =>
+                  // this item is after init message,
+                  // but it could be a valid json of generic message
+                  // so we can only log a message and ignore it
+                  debugger.debugMessage(s"expected a valid input message message. recieved invalid input. error: $error, input: $str") *> ZIO.none
+              }.collectSome)
+            // if decoding failed, send an error message to sender and log error
+            // because this was promised to be a init message, but was not,
+            // we will have to shut down the node
+            case Left(error) =>
+              handleInitDecodingError(genericMessage) *>
+                ZIO.fail(new Exception("init message decoding failed"))
+        case (Some(input, Left(error), _), remainder) =>
+          // same case as above, but this time, we dont have a generic message
+          // so we cant send an error message to sender
+          // we will have to shut down the node
+          debugger.debugMessage(s"expected init message. recieved invalid input. error: $error, input: $input") *>
+            ZIO.fail(new Exception("init message decoding failed"))
+        case (None, _) =>
+          // if we don't have a some yet, it means we didn't get any init message
+          // since we can't proceed without init message, its safe to fail the program
+          ZIO.fail(new Exception("no init message received"))
       }
 
   private def handleInit(message: Message[MaelstromInit]) =
@@ -61,14 +73,8 @@ case class InitializerLive(debugger: Debugger, transport: MessageTransport) exte
       _ <- debugger.debugMessage("initialised")
     } yield ()
 
-  private def handleInitDecoding(genericMessage: GenericMessage) =
+  private def handleInitDecodingError(genericMessage: GenericMessage) =
     debugger.debugMessage(s"could not decode init message $genericMessage") *>
       genericMessage
         .makeError(StandardErrorCode.MalformedRequest, "init message is malformed")
-        .fold(ZIO.unit)(transport.transport(_))
-
-  private def handleMessageOtherThanInit(message: GenericMessage) =
-    debugger.debugMessage(s"could not process message $message because node ${message.dest} is not initialised yet") *>
-      message
-        .makeError(StandardErrorCode.TemporarilyUnavailable, s"node ${message.dest} is not initialised yet")
         .fold(ZIO.unit)(transport.transport(_))
