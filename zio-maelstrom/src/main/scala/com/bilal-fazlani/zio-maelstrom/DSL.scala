7fbaae068fdef6cb52671bfa2dd5e7953e39e99e
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-05-31 16:29:00 +0530

    simplify DSL imple

--- zio-maelstrom/src/main/scala/com/bilal-fazlani/zio-maelstrom/DSL.scala
+++ zio-maelstrom/src/main/scala/com/bilal-fazlani/zio-maelstrom/DSL.scala
@@ -9,17 +9,17 @@ extension (s: String) infix def /(string: String): Path = Path.of(s, string)
 extension (p: Path) infix def /(string: String): Path = p resolve string
 
 extension [A <: MessageWithId](message: Message[A])
-  def reply[B <: MessageWithReply: JsonEncoder](out: B) = ZIO.serviceWithZIO[MessageSender](_.send(out, message.source))
+  def reply[B <: MessageWithReply: JsonEncoder](out: B) = MessageSender.send(out, message.source)
 
 extension (nodeId: NodeId)
   def ask[A <: MessageWithId: JsonEncoder, B <: MessageWithReply: JsonDecoder](body: A, timeout: Duration) = MessageSender.ask(body, nodeId, timeout)
   def send[A <: MessageBody: JsonEncoder](body: A) = MessageSender.send(body, nodeId)
 
-def broadcastAll[A <: MessageBody: JsonEncoder](body: A) = ZIO.serviceWithZIO[MessageSender](_.broadcastAll(body))
-def broadcastTo[A <: MessageBody: JsonEncoder](others: Seq[NodeId], body: A) = ZIO.serviceWithZIO[MessageSender](_.broadcastTo(others, body))
+def broadcastAll[A <: MessageBody: JsonEncoder](body: A) = MessageSender.broadcastAll(body)
+def broadcastTo[A <: MessageBody: JsonEncoder](others: Seq[NodeId], body: A) = MessageSender.broadcastTo(others, body)
 
 def me = ZIO.service[Context].map(_.me)
 def others = ZIO.service[Context].map(_.others)
 
-def logInfo(message: => String) = ZIO.serviceWithZIO[Logger](_.info(message))
-def logError(message: => String) = ZIO.serviceWithZIO[Logger](_.error(message))
+def logInfo(message: => String) = Logger.info(message)
+def logError(message: => String) = Logger.error(message)
