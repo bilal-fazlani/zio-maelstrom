eb88ffe0de6ea5ff0a2630c0260cb7cc92724a5f
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-06-03 16:56:43 +0530

    renames

--- examples/broadcast/src/main/scala/com/example/Main.scala
+++ examples/broadcast/src/main/scala/com/example/Main.scala
@@ -5,11 +5,11 @@ import zio.*
 import com.bilalfazlani.zioMaelstrom.protocol.*
 import com.bilalfazlani.zioMaelstrom.*
 
-@jsonDiscriminator("type") sealed trait BroadcastMessage derives JsonDecoder
-@jsonHint("topology") case class Topology(msg_id: MessageId, topology: Map[NodeId, List[NodeId]]) extends BroadcastMessage, NeedsReply
-@jsonHint("broadcast") case class Broadcast(message: Int, msg_id: MessageId)                      extends BroadcastMessage, NeedsReply
-@jsonHint("read") case class Read(msg_id: MessageId)                                              extends BroadcastMessage, NeedsReply
-@jsonHint("gossip") case class Gossip(iHaveSeen: Set[Int], `type`: String = "gossip") extends BroadcastMessage, Sendable derives JsonEncoder
+@jsonDiscriminator("type") sealed trait InMessage derives JsonDecoder
+@jsonHint("topology") case class Topology(msg_id: MessageId, topology: Map[NodeId, List[NodeId]]) extends InMessage, NeedsReply
+@jsonHint("broadcast") case class Broadcast(message: Int, msg_id: MessageId)                      extends InMessage, NeedsReply
+@jsonHint("read") case class Read(msg_id: MessageId)                                              extends InMessage, NeedsReply
+@jsonHint("gossip") case class Gossip(iHaveSeen: Set[Int], `type`: String = "gossip")             extends InMessage, Sendable derives JsonEncoder
 
 case class BroadcastOk(in_reply_to: MessageId, `type`: String = "broadcast_ok")           extends Sendable, Reply derives JsonEncoder
 case class ReadOk(messages: Set[Int], in_reply_to: MessageId, `type`: String = "read_ok") extends Sendable, Reply derives JsonEncoder
@@ -19,10 +19,8 @@ object Main extends ZIOAppDefault:
   val settings = Settings(logFormat = LogFormat.Colored)
 
   case class State(messages: Set[Int] = Set.empty, neighbours: Set[NodeId] = Set.empty) {
-    def addMessage(message: Int): State = copy(messages = messages + message)
-
-    def addGossip(newMessages: Set[Int], from: NodeId): State = copy(messages = messages ++ newMessages)
-
+    def addBroadcast(message: Int): State             = copy(messages = messages + message)
+    def addGossip(newMessages: Set[Int]): State       = copy(messages = messages ++ newMessages)
     def addNeighbours(neighbours: Set[NodeId]): State = copy(neighbours = neighbours)
   }
 
@@ -36,10 +34,10 @@ object Main extends ZIOAppDefault:
       .unit
   yield ()).delay(500.millis).forever
 
-  val handleMessages = receiveR[Ref[State], BroadcastMessage] {
+  val handleMessages = receiveR[Ref[State], InMessage] {
     case msg @ Broadcast(message, messageId) =>
       for
-        state <- ZIO.serviceWithZIO[Ref[State]](_.updateAndGet(_.addMessage(message)))
+        state <- ZIO.serviceWithZIO[Ref[State]](_.updateAndGet(_.addBroadcast(message)))
         _     <- msg reply BroadcastOk(messageId)
       yield ()
     case msg @ Read(messageId) =>
@@ -50,8 +48,8 @@ object Main extends ZIOAppDefault:
     case msg @ Topology(id, topology) =>
       for
         state <- ZIO.service[Ref[State]]
-        neighbours = topology(myNodeId)
-        _ <- state.update(_.addNeighbours(neighbours.toSet))
+        neighbours = topology(myNodeId).toSet
+        _ <- state.update(_.addNeighbours(neighbours))
         _ <- msg reply TopologyOk(id)
       yield ()
     case msg @ Gossip(iHaveSeen, _) =>
