af3f03ef6e78a7b87b3205dcd515f2e98813a859
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-06-03 19:09:56 +0530

    start gossip fiber only after topology has arrived

--- examples/broadcast/src/main/scala/com/example/Main.scala
+++ examples/broadcast/src/main/scala/com/example/Main.scala
@@ -6,7 +6,7 @@ import com.bilalfazlani.zioMaelstrom.protocol.*
 import com.bilalfazlani.zioMaelstrom.*
 
 @jsonDiscriminator("type") sealed trait InMessage derives JsonDecoder
-@jsonHint("topology") case class Topology(msg_id: MessageId, topology: Map[NodeId, List[NodeId]]) extends InMessage, NeedsReply
+@jsonHint("topology") case class Topology(topology: Map[NodeId, List[NodeId]], msg_id: MessageId) extends InMessage, NeedsReply
 @jsonHint("broadcast") case class Broadcast(message: Int, msg_id: MessageId)                      extends InMessage, NeedsReply
 @jsonHint("read") case class Read(msg_id: MessageId)                                              extends InMessage, NeedsReply
 @jsonHint("gossip") case class Gossip(iHaveSeen: Set[Int], `type`: String = "gossip")             extends InMessage, Sendable derives JsonEncoder
@@ -15,17 +15,18 @@ case class BroadcastOk(in_reply_to: MessageId, `type`: String = "broadcast_ok") 
 case class ReadOk(messages: Set[Int], in_reply_to: MessageId, `type`: String = "read_ok") extends Sendable, Reply derives JsonEncoder
 case class TopologyOk(in_reply_to: MessageId, `type`: String = "topology_ok")             extends Sendable, Reply derives JsonEncoder
 
-object Main extends ZIOAppDefault:
-  case class State(messages: Set[Int] = Set.empty, neighbours: Set[NodeId] = Set.empty) {
-    def addBroadcast(message: Int): State             = copy(messages = messages + message)
-    def addGossip(gossipMessages: Set[Int]): State    = copy(messages = messages ++ gossipMessages)
-    def addNeighbours(neighbours: Set[NodeId]): State = copy(neighbours = neighbours)
-  }
+case class State(messages: Set[Int] = Set.empty, neighbours: Set[NodeId] = Set.empty) {
+  def addBroadcast(message: Int): State             = copy(messages = messages + message)
+  def addGossip(gossipMessages: Set[Int]): State    = copy(messages = messages ++ gossipMessages)
+  def addNeighbours(neighbours: Set[NodeId]): State = copy(neighbours = neighbours)
+}
 
-  val startGossip = getState
-    .flatMap(state => ZIO.when(state.messages.nonEmpty && state.neighbours.nonEmpty)(gossip(state)))
-    .delay(500.millis)
-    .forever
+object Main extends ZIOAppDefault:
+
+  val getState                       = ZIO.serviceWithZIO[Ref[State]](_.get)
+  def updateState(f: State => State) = ZIO.serviceWithZIO[Ref[State]](_.update(f))
+
+  val startGossip = getState.flatMap(gossip).delay(500.millis).forever
 
   def gossip(state: State) =
     logInfo(s"sending gossip of size ${state.messages.size} to [${state.neighbours.mkString(",")}]") *>
@@ -34,25 +35,22 @@ object Main extends ZIOAppDefault:
         .withParallelism(5)
         .unit
 
-  val getState                       = ZIO.serviceWithZIO[Ref[State]](_.get)
-  def updateState(f: State => State) = ZIO.serviceWithZIO[Ref[State]](_.update(f))
-
-  val handleMessages = receiveR[Ref[State], InMessage] {
-    case msg @ Broadcast(message, messageId) =>
-      updateState(_.addBroadcast(message)) *> (msg reply BroadcastOk(messageId))
+  val handleMessages = receiveR[Ref[State] & Scope, InMessage] {
+    case msg @ Broadcast(broadcast, messageId) =>
+      updateState(_.addBroadcast(broadcast)) *> (msg reply BroadcastOk(messageId))
 
     case msg @ Read(messageId) =>
-      getState.map(_.messages) flatMap (myMessages => msg reply ReadOk(myMessages, messageId))
+      getState.map(_.messages) flatMap (messages => msg reply ReadOk(messages, messageId))
 
-    case msg @ Topology(messageId, topology) =>
+    case msg @ Topology(topology, messageId) =>
       val neighbours = topology(myNodeId).toSet
-      updateState(_.addNeighbours(neighbours)) *> (msg reply TopologyOk(messageId))
+      updateState(_.addNeighbours(neighbours)) *> (msg reply TopologyOk(messageId)) *> startGossip.forkScoped.unit
 
     case msg @ Gossip(gossipMessages, _) =>
       updateState(_.addGossip(gossipMessages))
   }
 
-  val run = (handleMessages race startGossip)
+  val run = handleMessages
     .provideSome[Scope](
       MaelstromRuntime.live,
       ZLayer.fromZIO(Ref.make(State()))
