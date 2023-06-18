6f5beea4fee1967f6a7ddec89deb11abd85ada61
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-06-18 12:16:01 +0530

    make logging strings byname params

--- zio-maelstrom/src/main/scala/com/bilal-fazlani/zio-maelstrom/Logger.scala
+++ zio-maelstrom/src/main/scala/com/bilal-fazlani/zio-maelstrom/Logger.scala
@@ -4,9 +4,9 @@ import zio.*
 import zio.Console.*
 
 private[zioMaelstrom] trait Logger:
-  def debug(line: String): UIO[Unit]
-  def info(line: String): UIO[Unit]
-  def error(line: String): UIO[Unit]
+  def debug(line: => String): UIO[Unit]
+  def info(line: => String): UIO[Unit]
+  def error(line: => String): UIO[Unit]
 
 private[zioMaelstrom] object Logger:
   val live = ZLayer.fromFunction { (settings: Settings) =>
@@ -20,21 +20,21 @@ private[zioMaelstrom] object Logger:
 private case class LoggerLive(settings: Settings) extends Logger:
   import zio.internal.ansi.AnsiStringOps
 
-  def debug(line: String): UIO[Unit] = ZIO
+  def debug(line: => String): UIO[Unit] = ZIO
     .when(settings.logLevel <= NodeLogLevel.Debug)(printLineError(line.faint).orDie).unit
 
-  def info(line: String): UIO[Unit] = ZIO
+  def info(line: => String): UIO[Unit] = ZIO
     .when(settings.logLevel <= NodeLogLevel.Info)(printLineError(line.yellow).orDie).unit
 
-  def error(line: String): UIO[Unit] = ZIO
+  def error(line: => String): UIO[Unit] = ZIO
     .when(settings.logLevel <= NodeLogLevel.Error)(printLineError(line.red).orDie).unit
 
 private object PlainLogger extends Logger:
-  def debug(line: String): UIO[Unit] = printLineError(line).orDie.unit
-  def info(line: String): UIO[Unit]  = printLineError(line).orDie.unit
-  def error(line: String): UIO[Unit] = printLineError(line).orDie.unit
+  def debug(line: => String): UIO[Unit] = printLineError(line).orDie.unit
+  def info(line: => String): UIO[Unit]  = printLineError(line).orDie.unit
+  def error(line: => String): UIO[Unit] = printLineError(line).orDie.unit
 
 private object DisabledLogger extends Logger:
-  def debug(line: String): UIO[Unit] = ZIO.unit
-  def info(line: String): UIO[Unit]  = ZIO.unit
-  def error(line: String): UIO[Unit] = ZIO.unit
+  def debug(line: => String): UIO[Unit] = ZIO.unit
+  def info(line: => String): UIO[Unit]  = ZIO.unit
+  def error(line: => String): UIO[Unit] = ZIO.unit
