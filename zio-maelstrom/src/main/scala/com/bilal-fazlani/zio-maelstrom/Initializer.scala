f9c14fff328f6dabcf1ec5c401994b7f0da9bc1d
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-07-01 20:11:32 +0530

    Add warning messages when init message does not arrive on startup

--- zio-maelstrom/src/main/scala/com/bilal-fazlani/zio-maelstrom/Initializer.scala
+++ zio-maelstrom/src/main/scala/com/bilal-fazlani/zio-maelstrom/Initializer.scala
@@ -4,6 +4,8 @@ import protocol.*
 import zio.*
 import zio.stream.{ZStream, ZSink}
 import zio.json.JsonDecoder
+import java.lang.{System => JSystem}
+import java.time.OffsetDateTime
 
 private[zioMaelstrom] type MessageStream = ZStream[Any, Nothing, GenericMessage]
 
@@ -25,9 +27,67 @@ private case class InitializerLive(
     outputChannel: OutputChannel,
     inputChannel: InputChannel
 ):
+  private val preInitMessages = for {
+    initialTime <- Clock.currentDateTime
+    _           <- logger.debug("node started. awaiting init message...")
+    schedule = Schedule.exponential(5.seconds, 2)
+    printFiber <- (ZIO.sleep(5.seconds) *> logInitDelayWarning(initialTime).repeat(schedule)).fork
+  } yield printFiber
+
+  private def logInitDelayWarning(initialTime: OffsetDateTime): ZIO[Any, Nothing, Unit] =
+    for {
+      currentTime <- Clock.currentDateTime
+      gap = Duration.fromMillis(
+        currentTime.toInstant.toEpochMilli - initialTime.toInstant.toEpochMilli
+      )
+      loggerf = gap match {
+        case gap if gap.toSeconds <= 10 => logger.info(_)
+        case gap if gap.toSeconds < 60  => logger.warn(_)
+        case _                          => logger.error(_)
+      }
+      _ <- loggerf(s"init message not received in ${renderDuration(gap)}")
+    } yield ()
+
+  private def renderDuration(duration: Duration): String = {
+    val seconds = duration.toSeconds()
+    val minutes = seconds / 60
+    val hours   = minutes / 60
+    val days    = hours / 24
+    val years   = days / 365
+    val months  = years / 12
+
+    val remainingSeconds = seconds % 60
+    val remainingMinutes = minutes % 60
+    val remainingHours   = hours   % 24
+    val remainingDays    = days    % 365
+    val remainingMonths  = months  % 12
+
+    val parts = Seq(
+      (remainingMonths, "month"),
+      (remainingDays, "day"),
+      (remainingHours, "hour"),
+      (remainingMinutes, "minute"),
+      (remainingSeconds, "second")
+    )
+
+    val nonZeroParts = parts.filter(_._1 > 0)
+
+    if (nonZeroParts.isEmpty) {
+      "0 seconds"
+    } else {
+      nonZeroParts
+        .map { case (value, unit) =>
+          s"$value ${unit}${if (value > 1) "s" else ""}"
+        }
+        .mkString(" ")
+    }
+  }
+
   val initialize: ZIO[Scope, Nothing, Initialisation] =
     for
-      inputs <- inputChannel.readInputs
+      warningFiber <- preInitMessages
+      _            <- ZIO.addFinalizer(warningFiber.interrupt)
+      inputs       <- inputChannel.readInputs
       init <- inputs.messageStream.peel(ZSink.head).flatMap {
         // happy case: found init message
         case (Some(genericMessage), remainder) =>
@@ -35,7 +95,7 @@ private case class InitializerLive(
           initMessage match
             // when decoded, send it to init handler
             case Right(initMessage) =>
-              handleInit(initMessage) as
+              handleInit(initMessage) *> warningFiber.interrupt as
                 Initialisation(Context(initMessage), Inputs(inputs.responseStream, remainder))
 
             // if decoding failed, send an error message to sender and log error
@@ -47,7 +107,7 @@ private case class InitializerLive(
         case (None, _) =>
           // if we don't have a some yet, it means we didn't get any init message
           // since we can't proceed without init message, its safe to fail the program
-          ZIO.die(new Exception("no init message received"))
+          ZIO.dieMessage("no init message received")
       }
     yield init
 
