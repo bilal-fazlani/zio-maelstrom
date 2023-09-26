23be1ac78dd9a5f57647ae54bf1caea97dfe8b09
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2023-09-26 15:52:45 +0530

    update zio and sbt versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Libs {
-  lazy val zioVersion = "2.0.15"
+  lazy val zioVersion = "2.0.18"
   private val ZIO     = "dev.zio"
 
   lazy val zio           = ZIO %% "zio"            % zioVersion
@@ -9,5 +9,5 @@ object Libs {
   lazy val zioStreams    = ZIO %% "zio-streams"    % zioVersion
   lazy val zioTest       = ZIO %% "zio-test"       % zioVersion
   lazy val zioTestSbt    = ZIO %% "zio-test-sbt"   % zioVersion
-  lazy val zioJson       = ZIO %% "zio-json"       % "0.5.0"
+  lazy val zioJson       = ZIO %% "zio-json"       % "0.6.2"
 }
