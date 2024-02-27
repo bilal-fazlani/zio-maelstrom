d463094060962772ef4ad0fab95eae9454d57682
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2024-02-27 22:45:38 +0530

    update link tso example

--- examples/echo/src/main/scala/com/example/TsoDocs.scala
+++ examples/echo/src/main/scala/com/example/TsoDocs.scala
@@ -4,9 +4,5 @@ import com.bilalfazlani.zioMaelstrom.*
 import zio.*
 
 object TsoExample {
-  val program: ZIO[MaelstromRuntime, AskError, Unit] = for
-    msgId1         <- Random.nextIntBounded(Int.MaxValue).map(MessageId(_))
-    timestamp: Int <- LinTso.ts(5.seconds)
-    _              <- logInfo(s"timestamp is $timestamp")
-  yield ()
+  val timestamp: ZIO[LinTso, AskError, Int] = LinTso.ts(5.seconds)
 }
