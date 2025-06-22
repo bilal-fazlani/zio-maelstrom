99a283cbca04a6e9587f965813b19c006ff4122d
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2025-06-23 00:45:02 +0100

    add more tests for ask patter

--- zio-maelstrom/src/test/scala/com/bilalfazlani/zioMaelstrom/AskTest.scala
+++ zio-maelstrom/src/test/scala/com/bilalfazlani/zioMaelstrom/AskTest.scala
@@ -24,22 +24,81 @@ object AskTest extends MaelstromSpec {
           .serviceWithZIO[MessageSender](_.ask[Ping, Pong](Ping("Hello"), NodeId("n2"), 1.second))
           .fork
         pingMessage: Message[Ping] <- getNextMessage[Ping]
-        _       <- inputReply(Pong("World"), NodeId("n2"), MessageId(1))
-        pong    <- pongFiber.join.debug("pong")
-        pingJson <- ZIO.from(pingMessage.toJsonAST).debug("pingJson")
+        _                          <- inputReply(Pong("World"), NodeId("n2"), MessageId(1))
+        pong                       <- pongFiber.join.debug("pong")
+        pingJson                   <- ZIO.from(pingMessage.toJsonAST).debug("pingJson")
         pingExpectedJson = Json(
           "src"  -> Json.Str("n1"),
           "dest" -> Json.Str("n2"),
           "body" -> Json(
-            "type"   -> Json.Str("ping"),
-            "text"   -> Json.Str("Hello")
+            "type" -> Json.Str("ping"),
+            "text" -> Json.Str("Hello")
           )
         )
         pongExpectedJson = Json(
-            "type"   -> Json.Str("pong"),
-            "text"   -> Json.Str("World")
-          )
+          "type" -> Json.Str("pong"),
+          "text" -> Json.Str("World")
+        )
       } yield assertTrue(true)
+    }.provide(tRuntime),
+    test("successfully send and receive message with 0 field each") {
+      case class Ping() derives JsonCodec
+      case class Pong() derives JsonCodec
+      for {
+        pongFiber <- ZIO
+          .serviceWithZIO[MessageSender](_.ask[Ping, Pong](Ping(), NodeId("n2"), 1.second))
+          .fork
+        pingMessage: Message[Ping] <- getNextMessage[Ping]
+        _                          <- inputReply(Pong(), NodeId("n2"), MessageId(1))
+        pong                       <- pongFiber.join.debug("pong")
+        pingJson                   <- ZIO.from(pingMessage.toJsonAST).debug("pingJson")
+        pingExpectedJson = Json(
+          "src"  -> Json.Str("n1"),
+          "dest" -> Json.Str("n2"),
+          "body" -> Json(
+            "type" -> Json.Str("ping")
+          )
+        )
+        pongExpectedJson = Json(
+          "type" -> Json.Str("pong")
+        )
+      } yield assertTrue(true)
+    }.provide(tRuntime),
+    test("successfully get and respond to a message with 1 field each") {
+      case class Ping(text: String) derives JsonCodec
+      case class Pong(text: String) derives JsonCodec
+      for {
+        fiber <- receive[Ping] { ping =>
+          reply(Pong("Hello " + ping.text))
+        }.timeout(500.millis).fork
+        _                          <- inputAsk(Ping("World"), NodeId("n2"), MessageId(1))
+        _                          <- TestClock.adjust(1.second) zipPar fiber.join.ignore
+        pongMessage: Message[Pong] <- getNextMessage[Pong]
+      } yield assertTrue(
+        pongMessage == Message(
+          NodeId("n1"),
+          NodeId("n2"),
+          Body(MsgName[Pong], Pong("Hello World"), None, Some(MessageId(1)))
+        )
+      )
+    }.provide(tRuntime),
+    test("successfully get and respond to a message with 0 field each") {
+      case class Ping() derives JsonCodec
+      case class Pong() derives JsonCodec
+      for {
+        fiber <- receive[Ping] { ping =>
+          reply(Pong())
+        }.timeout(500.millis).fork
+        _                          <- inputAsk(Ping(), NodeId("n2"), MessageId(1))
+        _                          <- TestClock.adjust(1.second) zipPar fiber.join.ignore
+        pongMessage: Message[Pong] <- getNextMessage[Pong]
+      } yield assertTrue(
+        pongMessage == Message(
+          NodeId("n1"),
+          NodeId("n2"),
+          Body(MsgName[Pong], Pong(), None, Some(MessageId(1)))
+        )
+      )
     }.provide(tRuntime)
   ) @@ TestAspect.timeout(10.seconds) @@ TestAspect.sequential
 }
